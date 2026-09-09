654b2eb08a34339a316bcb1c69fa390700135f62
Laura <ortizizquierdolaurakaria@gmail.com>
2026-09-09 10:26:00 -0500

    fix: Blindaje total de ASGI y Docker para solucionar no available server

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,15 +8,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+# Obtener la aplicación ASGI base primero
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import api.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             api.routing.websocket_urlpatterns
